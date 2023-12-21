ecf956dbab3c2c1892898200adf8a08dc8fde948
María Angélica Rojas Martínez <145722367+Mariangelicarojas@users.noreply.github.com>
2023-12-21 20:02:51 -0300

    Update index.js

--- js/index.js
+++ js/index.js
@@ -1,12 +1,5 @@
 function accionParaCuandoEllaDigaQueSi() {
-    const resultadoElemento = document.getElementById("resultado");
-
-    const respuesta = confirm("¿Estás segura que vas a comer CARNE DE VERDAD??");
-    if (respuesta){
-        resultadoElemento.innerHTML = "¡Perfecto! Disfrutemos de la hamburguesa carnivora juntos.";
-    } else {
-        resultadoElemento.innerHTML = "Te Amo";
-    }
+    alert("Vamos a comerrr HAMBURGUESA CARNIVORAAA")
 }
 
 function mueveElBoton() {
@@ -20,4 +13,27 @@ function mueveElBoton() {
     btnNo.style.position = "absolute";
     btnNo.style.left = newWidth + "px";
     btnNo.style.top = newHeight + "px";            
+}
+
+function accionParaCuandoEllaDigaQueSi() {
+    const respuesta = confirm("¿Estas segura que vas a comer CARNE DE VERDAD??");
+    if (respuesta){
+        window.location.href = "opcionsi.html";
+    }else{
+        alert("Te Amo")
+    }
+    
+}
+
+function mueveElBoton() {
+    const width = window.innerWidth;
+    const height = window.innerHeight;
+
+    const newWidth = Math.random() * width;
+    const newHeight = Math.random() * height;
+
+    const btnNo = document.getElementById("btnNo");
+    btnNo.style.position = "absolute";
+    btnNo.style.left = newWidth + "px";
+    btnNo.style.top = newHeight + "px";  
 }
